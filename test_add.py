0aa4259d2c906d76c37ceb0c42e5949e71dc5465
Noor Akram Alzoghby <N.Akram2204@nu.edu.eg>
2024-11-15 13:29:32 +0200

    Update test_add.py

--- test_add.py
+++ test_add.py
@@ -1,14 +1,14 @@
 import unittest
 
-from add import Add
+from add import add
 
 class Testadd(unittest.TestCase):
     def test_add(self):
-        result = Add.add(3, 7)
+        result = add.add(3, 7)
         self.assertEqual(result, 10)
 
-        result = Add.add(3, -7)
+        result = add.add(3, -7)
         self.assertEqual(result, -4)
 
-        result = Add.add(-3, -7)
+        result = add.add(-3, -7)
         self.assertEqual(result, -10)
